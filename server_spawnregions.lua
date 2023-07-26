8b54452b720382f4138262dab38412e9a517ed69
PepeCitron <1980308+PepeCitron@users.noreply.github.com>
2023-07-26 19:34:58 +0200

    Add default spawn locations

---
+++ server_spawnregions.lua
@@ -0,0 +1,8 @@
+function SpawnRegions()
+  return {
+    { name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+    { name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+    { name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+    { name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+  }
+  end
